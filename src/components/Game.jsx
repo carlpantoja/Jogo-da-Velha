70115ec1b08f644f446b07ccd2f46fb1a308f16f
Carlos Pantoja <carlos6.andre0@gmail.com>
2023-09-21 10:50:03 -0300

    modificações e adição do botão de novo jogo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,17 +6,14 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-top: 3em;`
-
-const H1 = styled.h1`
-    font-family: Arial, Helvetica, sans-serif;`
+    margin-top: 2em;`
 
 const Box = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 150px);
     grid-template-rows: repeat(3, 150px);
     gap: 1rem;
-    margin-top: 2em;
+    margin-top: 1em;
     padding: 15px;
     background-color: #5d8aa8;
     border-radius: 1em;
@@ -28,15 +25,56 @@ const Button = styled.button`
     padding: 10px;
     height: 1.5em;
     cursor: pointer;
+    border: none;
+    ${props => props.$clear && css `
+        height: 30px;
+        margin: 5px;
+        padding: 5px;
+        font-size: 15px;
+        border: 1px solid;
+        border-radius: 5px;`}
     ${props => props.$block && css`
-        cursor: not-allowed;`}`
+        cursor: not-allowed;`}
+    ${props => {
+        switch (props.$block){
+            case 'O':
+                return css`
+                background-color: #ffff00;`
+            case 'X':
+                return css`
+                background-color: #00ff00;`
+        }
+    }}
+    ${props => {
+        switch (props.$game){
+            case 'game-over':
+                return css`
+                cursor: not-allowed;`
+            default:
+                return null
+        }
+    }}`
+
+const Span = styled.div`
+    display: flex;
+    margin-top: 10px;`
+
+const Message = styled.p`
+    margin-top: 10px;
+    font-family: Arial, Helvetica, sans-serif;`
 
 function Game(){
     const emptyBlock = Array(9).fill('')
     const [block, setBlock] = useState(emptyBlock)
     const [currentPlayer, setCurrentPlayer] = useState('O')
+    const [winner, setWinner] = useState(null)
 
     const handleClick = (index) => {
+        if (winner) {
+            alert('Jogo finalizado!')
+            return null
+        }
+
         if (block[index] !== '') {
             alert('Posição ocupada!')
             return null
@@ -45,57 +83,52 @@ function Game(){
 
         setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
     }
-    // empate
-    const draw = () => {
-        if (block.every((item) => item !== '')) {
-            alert('Houve um empate')
-            resetGame()
-        }
-    }
 
-    function winner(){
-        const waysToWin = [
+    function result(){
+        const checkResult = [
             // vertical
             [block[0], block[1], block[2]],
             [block[3], block[4], block[5]],
             [block[6], block[7], block[8]],
-
             // horizontal
             [block[0], block[3], block[6]],
             [block[1], block[4], block[7]],
             [block[2], block[5], block[8]],
-
             // diagonal
             [block[0], block[4], block[8]],
             [block[2], block[4], block[6]]
         ]
 
-        waysToWin.forEach((cells) => {
-            if (cells.every((cell) => cell === 'O')) {
-                alert('O venceu!')
-                resetGame()
-            }
-            if (cells.every((cell) => cell === 'X')) {
-                alert('X venceu!')
-                resetGame()
-            }
+        checkResult.forEach((block) => {
+            if (block.every((item) => item === 'O')) setWinner('O')
+            if (block.every((item) => item === 'X')) setWinner('X')
         })
+
+        if (block.every((item) => item !== '')) setWinner('D')
     }
 
     const resetGame = () => {
         setCurrentPlayer('O')
         setBlock(emptyBlock)
+        setWinner(null)
     }
 
-    useEffect(winner, [block])
-    useEffect(draw, [block])
+    useEffect(result, [block])
 
     return(
         <Container>
-            <H1>Jogo da Velha</H1>
             <Box>
-                {block.map((item, index) => (<Button $block={`${item}`} key={String(index)} onClick={() => handleClick(index)}>{item}</Button>))}
+                {block.map((item, index) => 
+                    (<Button $block={`${item}`} $game={`${winner ? 'game-over' : ''}`} key={String(index)} onClick={() => handleClick(index)}>{item}</Button>))
+                }
             </Box>
+            {winner &&
+                <Span>
+                    {winner === 'D' ? <Message>Deu empate</Message>
+                    : <Message>{winner} venceu!</Message>}
+                    <Button $clear onClick={resetGame}>Novo Jogo</Button>
+                </Span>
+            }
         </Container>
     )
 }
